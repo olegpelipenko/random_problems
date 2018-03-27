ff9a86b475cd8e973199b8effe3386a5c2f2d289
fryme <oleg.fry@gmail.com>
2018-03-27 11:39:54 +0300

    Bugfix

--- redis_lock_pubsub/redis_lock_pubsub.go
+++ redis_lock_pubsub/redis_lock_pubsub.go
@@ -39,9 +39,11 @@ func sendMessagesToClient(channel string, client * redis.Client, messageSleepTim
 	for {
 		time.Sleep(messageSleepTimeout)
 		message := generateRandomString(6)
+
 		res, err := client.Publish(channel, message).Result()
 		if err != nil {
-			log.Fatal("Failed to publish message", err)
+			log.Println("Failed to publish message", err)
+			continue
 		}
 
 		if res == 0 {
@@ -50,6 +52,96 @@ func sendMessagesToClient(channel string, client * redis.Client, messageSleepTim
 		} else {
 			log.Println("Message", message, "for", channel, "was sent", res)
 		}
+	}
+}
+
+func tryToAcquirePublisherLock(client * redis.Client, redisQueueLock string, roleSwitchChannel * chan struct{}) {
+	for {
+		ttl, err := client.TTL(redisQueueLock).Result()
+		if err != nil {
+			log.Println("Error in TTL command:", err)
+		}
+		time.Sleep(ttl)
+
+		currentLock, err := client.Exists(redisQueueLock).Result()
+		log.Println("currentlock", currentLock)
+		if err != nil {
+			log.Println("Error getting lock", err)
+			continue
+		}
+		if currentLock == 0 {
+			log.Println("I'm trying to became a pulisher")
+			close(*roleSwitchChannel)
+			break
+		}
+	}
+}
+
+func printErrors(client * redis.Client, redisErrorsQueue string) {
+	var errRange []string
+	popAllErrors := func(errRange * []string) error {
+		err := client.Watch(func(tx *redis.Tx) error {
+			var err error
+			*errRange, err = tx.LRange(redisErrorsQueue, 0, -1).Result()
+			if err != nil && err != redis.Nil {
+				return err
+			}
+
+			_, err = tx.Pipelined(func(pipe redis.Pipeliner) error {
+				pipe.Del(redisErrorsQueue)
+				return nil
+			})
+			return err
+		})
+		if err == redis.TxFailedErr {
+			return popAllErrors(errRange)
+		}
+		return err
+	}
+
+	if err := popAllErrors(&errRange); err != nil {
+		log.Fatalf("error while getting errors list: '%v'", err)
+	}
+
+	log.Println("Errors:")
+	for _, error := range errRange {
+		log.Println(error)
+	}
+}
+
+func hearthbeat(client * redis.Client, redisQueueLock string, redisLockTimeout time.Duration, redisMyId string) {
+	for {
+		time.Sleep(redisLockTimeout - time.Second)
+
+		// Transaction to refresh my timeout on a lock
+		refreshLockTimeout := func(myId string) error {
+			err := client.Watch(func(tx *redis.Tx) error {
+				holderId, err := tx.Get(redisQueueLock).Bytes()
+				if err != nil && err != redis.Nil {
+					return err
+				}
+
+				if string(holderId) == myId {
+					_, err = tx.Pipelined(func(pipe redis.Pipeliner) error {
+						pipe.Set(redisQueueLock, myId, redisLockTimeout)
+						log.Println("Lock was refreshed")
+						return nil
+					})
+				} else {
+					log.Println("Lock holder changed")
+					//pubsub.Unsubscribe(redisClientsChannelsPattern)
+					//pubsub.Close()
+					return errors.New("lock holder changed")
+				}
+				return err
+			}, myId)
+			if err == redis.TxFailedErr {
+				return refreshLockTimeout(myId)
+			}
+			return err
+		}
+
+		refreshLockTimeout(redisMyId)
 	}
 }
 
@@ -81,42 +173,13 @@ func main() {
 	defer client.Close()
 
 	if *getError {
-		var errRange []string
-		popAllErrors := func(errRange * []string) error {
-			err := client.Watch(func(tx *redis.Tx) error {
-				var err error
-				*errRange, err = tx.LRange(redisErrorsQueue, 0, -1).Result()
-				if err != nil && err != redis.Nil {
-					return err
-				}
-
-				_, err = tx.Pipelined(func(pipe redis.Pipeliner) error {
-					pipe.LRem(redisErrorsQueue, 0, -1)
-					return nil
-				})
-				return err
-			})
-			if err == redis.TxFailedErr {
-				return popAllErrors(errRange)
-			}
-			return err
-		}
-
-		if err := popAllErrors(&errRange); err != nil {
-			log.Fatalf("error while getting errors list: '%v'", err)
-		}
-
-		log.Println("Errors:")
-		for _, error := range errRange {
-			log.Println(error)
-		}
-
+		printErrors(client, redisErrorsQueue)
 		return
 	}
 
 	// Should be unique between all clients
 	redisMyId := "client_" + generateRandomString(6) + "_" + strconv.FormatInt(time.Now().Unix(), 10)
-	log.Println("Trying to acquire lock, my id:", redisMyId)
+	log.Println("Trying to became publisher, my id:", redisMyId)
 
 	for {
 		cmd := client.SetNX(redisQueueLock, redisMyId, redisLockTimeout)
@@ -128,40 +191,9 @@ func main() {
 			log.Println("I'm publisher")
 
 			// Hearthbeat
-			go func() {
-				for {
-					time.Sleep(time.Second)
-					// Transaction to refresh my timeout on a lock
-					refreshLockTimeout := func(myId string) error {
-						err := client.Watch(func(tx *redis.Tx) error {
-							holderId, err := tx.Get(redisQueueLock).Bytes()
-							if err != nil && err != redis.Nil {
-								return err
-							}
+			go hearthbeat(client, redisQueueLock, redisLockTimeout, redisMyId)
 
-							if string(holderId) == myId {
-								_, err = tx.Pipelined(func(pipe redis.Pipeliner) error {
-									pipe.Set(redisQueueLock, myId, redisLockTimeout)
-									log.Println("Lock was refreshed")
-									return nil
-								})
-							} else {
-								log.Println("Lock holder changed")
-								//pubsub.Unsubscribe(redisClientsChannelsPattern)
-								//pubsub.Close()
-								return errors.New("lock holder changed")
-							}
-							return err
-						}, myId)
-						if err == redis.TxFailedErr {
-							return refreshLockTimeout(myId)
-						}
-						return err
-					}
-					refreshLockTimeout(redisMyId)
-				}
-			}()
-
+			// Run message sender for already existing clients
 			channels, err := client.PubSubChannels(redisClientsChannelsPattern).Result()
 			if err != nil {
 				log.Fatal("Errror listing channels", err)
@@ -169,72 +201,52 @@ func main() {
 			for _, channel := range channels {
 				go sendMessagesToClient(channel, client, messageSleepTimeout)
 			}
-			pubsub := client.Subscribe(redisClientsChannelsPattern)
 
+			newClientsSub := client.Subscribe(redisClientsChannelsPattern)
 			for {
-				msg, err := pubsub.ReceiveMessage()
+				msg, err := newClientsSub.ReceiveMessage()
 				if err != nil {
 					log.Println("Receiving client error:", err)
 					continue
 				}
 
-				log.Println("received", msg.Payload, "from", msg.Channel)
-
-				// New client
-				channel := msg.Payload
-				go sendMessagesToClient(channel, client, messageSleepTimeout)
+				log.Println("New client", msg.Payload)
+				go sendMessagesToClient(msg.Payload, client, messageSleepTimeout)
 			}
+
+			newClientsSub.Unsubscribe(redisClientsChannelsPattern)
+			newClientsSub.Close()
 		} else {
 			log.Println("I'm subscriber")
 
-			// Registering
+			// Registering myself in a message clients queue
 			_, err := client.Publish(redisClientsChannelsPattern, redisMyId).Result()
 			if err != nil {
 				log.Fatal("Failed to register:", err)
 			}
 
-			msgPubsub := client.Subscribe(redisMyId)
+			msgSub := client.Subscribe(redisMyId)
+			roleSwitchChannel := make(chan struct{})
 
-			c := make(chan int)
-			// Trying to acquire publisher lock
-			go func() {
-				for {
-					ttl, err := client.TTL(redisQueueLock).Result()
-					if err != nil {
-						log.Println("Error in TTL command:", err)
-					}
-					time.Sleep(ttl)
-
-					currentLock, err := client.Exists(redisQueueLock).Result()
-					log.Println("currentlock", currentLock)
-					if err != nil {
-						log.Println("Error getting lock", err)
-						continue
-					}
-					if currentLock == 0 {
-						log.Println("I'm trying to became a pulisher")
-						close(c)
-						break
-					}
-				}
-			}()
+			go tryToAcquirePublisherLock(client, redisQueueLock, &roleSwitchChannel)
 
 			// Receiving messages until publisher exists
 			for {
 				select {
-				case _, ok := <- c:
+				case _, ok := <- roleSwitchChannel:
 					if !ok {
-						c = nil
+						roleSwitchChannel = nil
 					}
 				default:
-					msg, err := msgPubsub.ReceiveTimeout(time.Second)
+					msg, err := msgSub.ReceiveTimeout(time.Second)
 					if err != nil {
 						log.Println("Receiving message error:", err)
 					} else {
 						log.Println("Message received", msg)
+
 						isError := rand.Intn(100) < 5
 						if isError {
-							log.Println("This is an error:", msg)
+							log.Println("This message is an error:", msg)
 
 							_, err = client.LPush(redisErrorsQueue, msg).Result()
 							if err != nil {
@@ -243,10 +255,11 @@ func main() {
 						}
 					}
 				}
-				if c == nil {
-					log.Println("Break")
-					msgPubsub.Unsubscribe(redisMyId)
-					msgPubsub.Close()
+
+				if roleSwitchChannel == nil {
+					log.Println("Trying to change role to publisher")
+					msgSub.Unsubscribe(redisMyId)
+					msgSub.Close()
 					break
 				}
 			}
